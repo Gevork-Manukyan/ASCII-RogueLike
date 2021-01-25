20efc908f58de818cb039a6b4499567a555e3682
Gevork Manukyan <Gevork.Manukyan.297@my.csun.edu>
2021-01-24 18:57:25 -0800

    Update Chest.cpp

--- ASCII RogueLike V2/Chest.cpp
+++ ASCII RogueLike V2/Chest.cpp
@@ -1,5 +1,7 @@
 #include "Chest.h"
 #include <iostream>
+#include <conio.h>
+
 
 //A chest with a status true has not been openned
 
@@ -36,4 +38,5 @@ void Chest::openChest(Player& player) {
 	std::cout << "You obtained a: " << _item.getName();
 	player.addItem(&_item);
 	_status = false;
+	_getch();
 }
